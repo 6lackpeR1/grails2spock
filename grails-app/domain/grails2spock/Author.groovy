e8aa787d6a65ffad46c9259c66dcc49d28dc1b1d
roofimon <roofimon@gmail.com>
2012-01-09 17:41:01 +0700

    Done all spec and test Author class

---
+++ grails-app/domain/grails2spock/Author.groovy
@@ -0,0 +1,14 @@
+package grails2spock
+class Author{
+	String firstname
+	String lastname
+	
+	static constraints = {
+		firstname(blank:false, maxSize:20)
+		lastname(blank:false, maxSize:20)
+	}
+	
+	String toString(){
+		"ID=$id:$firstname $lastname"
+	}
+}
---
+++ test/unit/grails2spock/AuthorSpec.groovy
@@ -0,0 +1,72 @@
+package grails2spock
+
+import grails.plugin.spock.*
+import spock.lang.Unroll
+
+class AuthorSpec extends UnitSpec{
+	def "find author by firstname and lastname"() {
+    	setup:
+    		mockDomain(Author)
+
+    	when:
+    		new Author(firstname: firstname, lastname: lastname).save()
+
+    	then:
+    		Author.findByFirstnameAndLastname(firstname, lastname) != null
+
+    	where:
+    		firstname = "John"
+    		lastname = "Doe"
+ 	}
+
+	@Unroll({"test firstname all constraints: $firstname is $valid"})	
+	def "firstname constraints"(){
+		setup:
+			mockForConstraintsTests(Author)
+		
+		when:
+			def author = new Author(firstname: firstname, lastname: "Panichsombat")
+			author.validate()
+		
+		then:
+			author.hasErrors() == !valid
+			
+	    where:
+			firstname               | valid
+			"123456789012345678901" | false
+			"12345678901234567890"  | true
+			""                      | false
+	}
+	
+	@Unroll({"test lastname all constraints: $lastname is $valid"})
+	def "lastname constraints"(){
+		setup:
+			mockForConstraintsTests(Author)
+		
+		when:
+			def author = new Author(firstname:"Twin", lastname: lastname)
+			author.validate()
+		then:
+			author.hasErrors() == !valid
+			
+		where:
+			lastname				| valid
+			""						| false
+			"123456789012345678901" | false
+			"12345678901234567890"	| true
+	}
+	
+	def "valid toString"(){
+		setup:
+			mockDomain(Author)
+		
+		when:
+			def author = new Author(firstname:"Twin", lastname:"Panichsombat").save()
+		
+		then: 	
+		    author.toString() == "ID=2:Twin Panichsombat"
+	}
+
+
+	
+}
