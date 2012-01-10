5431fdd318c86e8ac03122cf4b34f3503ec67ce3
roofimon <roofimon@gmail.com>
2012-01-10 18:11:18 +0700

    Refactor Book Spec

--- grails-app/domain/grails2spock/Book.groovy
+++ grails-app/domain/grails2spock/Book.groovy
@@ -1,10 +1,12 @@
 package grails2spock
 class Book{
 	String title
+	String isbn
 	static belongsTo = [author: Author]
 	
 	static constraints = {
 		title(blank:false, maxSize:50)
+		isbn(blank:false, maxSize:10)
 	}
 	
 	String toString(){
--- test/unit/grails2spock/BookSpec.groovy
+++ test/unit/grails2spock/BookSpec.groovy
@@ -2,28 +2,52 @@ package grails2spock
 import grails.plugin.spock.*
 import spock.lang.Unroll
 class BookSpec extends UnitSpec{
-	def "find book by title"(){
-		setup:
-			mockDomain(Book)
-			mockDomain(Author)
-			
+	def dummyAuthor// = new Author(firstname:"Twin", lastname:"Panichsombat")
+	def dummyBook  // = new Book(title:"", isbn:"")
+
+	def setup(){
+		dummyAuthor = new Author(firstname:"Twin", lastname:"Panichsombat")
+		dummyBook   = new Book(title:"Lean", isbn:"01234567890")		
+		dummyBook.author = dummyAuthor
+	}
+
+	def "book should have all these attributes"(){		
 		when:
-			new Book(title: title, author: new Author(firstname:"John", lastname:"Doe").save()).save()
+			def book   = dummyBook
+
+		then:
+			book.title == dummyTitle	
+			book.isbn  == dummyIsbn
+
+		where:
+			dummyTitle = "Lean"
+			dummyIsbn  = "01234567890"	
+	}
+
+	def "find book by title"(){		
+		setup:
+			mockDomain(Book, [dummyBook])	
+			mockDomain(Author, [dummyAuthor])	
+
+		when:			
+			def book = dummyBook
+			book.save()
 			
 		then:
-			Book.findByTitle(title) != null
+			def targetBook = Book.findByTitle(dummyTitle)
+			targetBook != null
+			targetBook.author.firstname == dummyAuthorFirstname
+
 		
 		where:
-			title = "Lean"
+			dummyTitle = "Lean"
+			dummyIsbn  = "01234567890"	
+			dummyAuthorFirstname = "Twin"
 	}
 	
 	def "book should display it's title as a result of toString"(){
-		setup:
-			mockDomain(Book)
-			mockDomain(Author)
-			
 		when:
-			def book = new Book(title: title, author: new Author(firstname:"Twin", lastname:"Panichsombat"))
+			def book = dummyBook
 			
 		then:
 			book.toString() == title
@@ -34,36 +58,54 @@ class BookSpec extends UnitSpec{
 	
 	def "book title should not longer than 50 characters"(){
 		setup:
-			mockForConstraintsTests(Book)
+			mockForConstraintsTests(Book, [dummyBook])
 			mockDomain(Author)
 			
 		when:
-			def book = new Book(title: title, author: new Author(firstname:"Twin", lastname:"Panichsombat").save())
+			def book = dummyBook
+			book.title = invalidBookTitle			
 			book.validate()
 
 		then:
 			book.errors.hasFieldErrors("title")
 			
 		where:
-			title = "123456789012345678901234567890123456789012345678901"
+			invalidBookTitle = "123456789012345678901234567890123456789012345678901"
 			
 	}
 	
 	def "book title should not be blank"(){
 		setup:
-			mockForConstraintsTests(Book)
+			mockForConstraintsTests(Book, [dummyBook])
 			mockDomain(Author)
 			
 		when:
-			def book = new Book(title: title, author: new Author(firstname:"Twin", lastname:"Panichsombat").save())
+			def book = dummyBook
+			book.title = blankTitle
 			book.validate()
 			
 		then:
 			book.errors.hasFieldErrors("title")
 			
 		where:
-			title = ""
+			blankTitle = ""
 	}
 	
+	def "book isbn must not longer than 10 digit"(){
+		setup:
+			mockForConstraintsTests(Book, [dummyBook])
+			mockDomain(Author)
+			
+		when:
+			def book = dummyBook
+			book.isbn = invalidIsbn
+			book.validate()
+
+		then:
+			book.errors.hasFieldErrors("isbn")
+			
+		where:
+			invalidIsbn = "12345678901"		
+	}
 	
 }
